59da07919c49b877f647808bb793d17f0ad9ebef
Pavel <pavel20042019@mail.ru>
2025-08-06 16:39:03 +0700

    functionsphp

--- functions.php
+++ functions.php
@@ -5,6 +5,6 @@ function wpBPM_enqueue_scripts() {
 
     wp_enqueue_style('style-css', get_stylesheet_uri());
 }
-add_action('wp_enqueue_scripts', 'wpblog_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'wpBPM_enqueue_scripts');
 
 ?>
