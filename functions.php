7cc797f60be0ebed577e089fd9d0734719d07141
Pavel <pavel20042019@mail.ru>
2025-08-08 16:46:43 +0700

    testCommit

--- functions.php
+++ functions.php
@@ -1,6 +1,14 @@
 
 <?php
 
+// function wpBPM_custom_header_setup() {
+//         add_theme_support('custom-header', array (
+//         'width' => 100%,
+//         'height' => 300
+//         ))
+// }
+
+
 function wpBPM_enqueue_scripts() {
 
     wp_enqueue_style('style-css', get_stylesheet_uri());
--- index.php
+++ index.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
     <?php get_header()?>
 
-        <h1>321312</h1>
 
-      <?php get_footer()?>
-</body>
-</html>
+
+     <?php get_footer()?>
