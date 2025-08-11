e5c5c2e136c9c678f37aee0141ab466e8178919f
Pavel <pavel20042019@mail.ru>
2025-08-12 00:20:00 +0700

    hero-adaptive

--- template-parts/main-section.php
+++ template-parts/main-section.php
@@ -1,10 +1,9 @@
 <main class="hero">
 
-<div class="main-info">
-    <h1 class="main-title">Цифровизация <br>
-        это просто</h1>
-    <p class="main-subtitle">Автоматизируйте процессы в одной команде со специалистами low-code</p>
-    <button class="main-button">Узнать подробнее</button>
+<div class="hero-info">
+    <h1 class="hero-title">Цифровизация <br> это просто</h1>
+    <p class="hero-subtitle">Автоматизируйте процессы в одной команде со специалистами low-code</p>
+    <button class="hero-button">Узнать подробнее</button>
 </div>
 
 </main>
