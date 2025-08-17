05566a1f4b4bcbfac394cf1507f12429de34f3f1
Pavel <pavel20042019@mail.ru>
2025-08-17 19:36:55 +0700

    header-scf-menu

---
+++ assets/js/header/header-scroll-effect.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let header = document.querySelector("header");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+        }else {
+            header.classList.remove("scrolled");
+        }
+    })
+
+})
+
--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset')?>">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>  <?php bloginfo('name'); ?></title>
       <?php wp_head(); ?>
 </head>
 <body>
@@ -14,21 +14,39 @@
           <div class="header-left-block">
                <h1 class="header-title">
                   <a class="header-title" href="<?php echo esc_url(home_url('/')); ?>">
-                    <?php bloginfo('name'); ?>
+                   <?php
+                   $field = get_field("header_logo_text");
+                        if ($field):
+                       echo esc_html($field);
+                        endif;
+
+                   ?>
                   </a>
                 </h1>
              </div>
-          <nav class="header-navbar">
-                   <?php
-                   wp_nav_menu(array(
-                       'theme_location' => 'header_menu',
-                       'container'      => false,
-                       'menu_class'     => 'header-menu',
-                       'link_before'    => '<span class="header-menu__text">',
-                       'link_after'     => '</span>',
-                   ));
-                    ?>
-               </nav>
+
+
+              <nav class="header-menu header-navbar">
+            <?php
+              $links_group = get_field("header_menu_links");
+               if ($links_group && is_array($links_group)):
+               foreach($links_group as $link):
+                 ?>
+
+                        <a target="_blank" href="<?php echo esc_url($link['menu_item_url']); ?>" class="header-menu__text">
+                        <?php echo esc_html($link["menu_item_label"]);?>
+                        <span>
+                        <?php echo $link["menu_link_svg"];?>
+                        </span>
+                        </a>
+
+                        <?php
+                          endforeach;
+                         endif;
+
+                        ?>
+
+                   </nav>
 
               <button id="burger-menu-btn" class="burger-menu-btn">
                        <div class="burger-menu-line"></div>
@@ -40,22 +58,40 @@
                          <div class="header-left-block">
                                      <h1 class="header-title">
                                         <a class="header-title" href="<?php echo esc_url(home_url('/')); ?>">
-                                          <?php bloginfo('name'); ?>
+                                            <?php
+                                         $field = get_field("header_logo_text");
+                                                    if ($field):
+                                                  echo esc_html($field);
+                                                endif;
+                                             ?>
                                         </a>
                                       </h1>
                                    </div>
-                          <?php
-                                        wp_nav_menu(array(
-                                              'theme_location' => 'header_menu',
-                                              'container'      => false,
-                                              'menu_class'     => 'header-burger-menu',
-                                              'link_before'    => '<span class="header-menu__text">',
-                                              'link_after'     => '</span>',
-                                          ));
-                          ?>
+
+                               <div class="header-burger-menu">
+                                     <?php
+                                                 $links_group = get_field("header_menu_links");
+                                                  if ($links_group && is_array($links_group)):
+                                                  foreach($links_group as $link):
+                                                    ?>
+
+                                                           <a target="_blank" href="<?php echo esc_url($link['menu_item_url']); ?>" class="header-menu__text">
+                                                           <?php echo esc_html($link["menu_item_label"]);?>
+                                                             <span>
+                                                              <?php echo $link["menu_link_svg"];?>
+                                                             </span>
+                                                           </a>
+
+                                                           <?php
+                                                             endforeach;
+                                                            endif;
+                                             ?>
+                               </div>
                    </nav>
 
        </div>
+
+
 
 
 
