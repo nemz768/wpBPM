c6485287e68c653e416d371ba8d2bc644f9fdbca
Оноприенко Анжелика <anjik14.01@gmail.com>
2025-08-27 22:54:07 +0700

    Как строится работа? - Правки

--- template-parts/about-us-in-numbers.php
+++ template-parts/about-us-in-numbers.php
@@ -2,34 +2,34 @@
 
 $about_us_data = [
     [
-        'number' => '300+',
+        'about_us_in_numbers_number' => '300+',
         'number_class' => 'item_1_numbers',
-        'title' => 'Обученных low-code разработчиков на elma',
-        'description' => 'Наш центр обучения выпустил более 300 подготовленных с 0 специалистов по ELMA365, как для компаний-пользователей ELMA365, так и для пополнения нашей дружной команды во всех городах присутствия.'
+        'about_us_in_numbers_header' => 'Обученных low-code разработчиков на elma',
+        'about_us_in_numbers_description' => 'Наш центр обучения выпустил более 300 подготовленных с 0 специалистов по ELMA365, как для компаний-пользователей ELMA365, так и для пополнения нашей дружной команды во всех городах присутствия.'
     ],
     [
-        'number' => '60+',
+        'about_us_in_numbers_number' => '60+',
         'number_class' => 'item_1_numbers',
-        'title' => 'сертифицированных специалистов elma',
-        'description' => 'В настоящее время в компании работает более 60 сертифицированных по ELMA365 специалистов, количество которых постоянно растет вместе с появлением новых проектов.'
+        'about_us_in_numbers_header' => 'сертифицированных специалистов elma',
+        'about_us_in_numbers_description' => 'В настоящее время в компании работает более 60 сертифицированных по ELMA365 специалистов, количество которых постоянно растет вместе с появлением новых проектов.'
     ],
     [
-        'number' => '100+',
+        'about_us_in_numbers_number' => '100+',
         'number_class' => 'item_2_numbers',
-        'title' => 'проектов на elma',
-        'description' => 'С 2017 года нами реализовано более 100 проектов по автоматизации бизнес-процессов и цифровой трансформации более чем 40 компаний.'
+        'about_us_in_numbers_header' => 'проектов на elma',
+        'about_us_in_numbers_description' => 'С 2017 года нами реализовано более 100 проектов по автоматизации бизнес-процессов и цифровой трансформации более чем 40 компаний.'
     ],
     [
-        'number' => '8',
+        'about_us_in_numbers_number' => '8',
         'number_class' => 'item_2_numbers',
-        'title' => 'лет автоматизируем процессы на elma',
-        'description' => 'Компания создана в 2014 году, с 2017 года мы специализируемся исключительно на проектах комплексной цифровой трансформации бизнеса, оптимизации бизнес-процессов и их подготовке к автоматизации при помощи BPMS (Business Process Management System).'
+        'about_us_in_numbers_header' => 'лет автоматизируем процессы на elma',
+        'about_us_in_numbers_description' => 'Компания создана в 2014 году, с 2017 года мы специализируемся исключительно на проектах комплексной цифровой трансформации бизнеса, оптимизации бизнес-процессов и их подготовке к автоматизации при помощи BPMS (Business Process Management System).'
     ],
     [
-        'number' => 'platinum',
+        'about_us_in_numbers_number' => 'platinum',
         'number_class' => 'item_3_numbers',
-        'title' => 'партнер elma',
-        'description' => 'Глубокая экспертиза в области бизнес-процессов и цифровой трансформации компаний, а также экспертное владение технологическим стеком ELMA365, позволяют нам с гордостью носить титул Платинового партнера ELMA.'
+        'about_us_in_numbers_header' => 'партнер elma',
+        'about_us_in_numbers_description' => 'Глубокая экспертиза в области бизнес-процессов и цифровой трансформации компаний, а также экспертное владение технологическим стеком ELMA365, позволяют нам с гордостью носить титул Платинового партнера ELMA.'
     ]
 ];
 ?>
@@ -39,12 +39,20 @@ $about_us_data = [
     <div class="about_us_in_numbers">
         <?php foreach ($about_us_data as $item): ?>
             <div class="number_block">
+
+                <?php
+                   $field = get_field("about_us_in_numbers");
+                        if ($field):
+                       echo esc_html($field);
+                        endif;
+                ?>
+
                 <div class="<?php echo esc_attr($item['number_class']); ?>">
-                    <h3><?php echo esc_html($item['number']); ?></h3>
+                    <h3><?php echo esc_html($item['about_us_in_numbers_number']); ?></h3>
                 </div>
                 <div class="text">
-                    <h4><?php echo esc_html($item['title']); ?></h4>
-                    <p><?php echo esc_html($item['description']); ?></p>
+                    <h4><?php echo esc_html($item['about_us_in_numbers_header']); ?></h4>
+                    <p><?php echo esc_html($item['about_us_in_numbers_description']); ?></p>
                 </div>
             </div>
         <?php endforeach; ?>
